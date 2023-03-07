09037e1e8357e9585a35c4fc2496ab6e0fd33935
Wendell Miranda <wendell.sacramento@tce.ap.gov.br>
2023-03-07 12:58:42 -0300

    ajustando a tabela viagem

--- src/viagem/dto/create-viagem.dto.ts
+++ src/viagem/dto/create-viagem.dto.ts
@@ -1,14 +1,11 @@
 export class CreateViagemDto {
-  /* origem_id: number;
-    destino_id: number;
-    trecho: string;
-    data_ida: Date;
-    data_volta?: Date;
-    datareg: Date;
-    justificativa?: string; */
 
   origem_id: number;
   destino_id: number;
+
+  cidade_origem_id: number;
+  cidade_destino_id: number;
+
   exterior: string;
   local_exterior: string;
   pais_id: number;
