a73f93bee73d254d2b68b4a57c70295d5a049fa4
Wendell Miranda <wendell.sacramento@tce.ap.gov.br>
2023-09-18 13:19:20 -0300

    separando a lógica das diárias

---
+++ src/calculo_diarias/diarias-enum.ts
@@ -0,0 +1,12 @@
+
+export enum Acompanha {
+    DENTRO = 766.22,
+    FORA = 851.36,
+    INTERNACIONAL = 472.55,
+  } 
+  
+  export enum Local {
+    DENTRO = 'dentro',
+    FORA = 'fora',
+    INTERNACIONAL = 'internacional',
+  }
--- src/calculo_diarias/estadual.ts
+++ src/calculo_diarias/estadual.ts
@@ -1,5 +1,43 @@
-
+import { Util } from "src/util/Util";
+import { Acompanha, Local } from "./diarias-enum";
+import { valor_diarias, viagem } from '@prisma/client';
+import { evento } from '@prisma/client';
 
 export default class CalculoEstadual {
+
+
+    servidores(viagem: viagem, uf: string, valorDiaria: valor_diarias, evento: evento): number {    
+        const totalDias = Util.totalDeDias(evento.inicio, evento.fim)+2;
+        const diarias = totalDias - 1;
+    
+        if (uf === 'AP' && viagem.viagem_pernoite === "SIM" || viagem.viagem_superior === "SIM") {
+          const meiaDiaria = this.valorServidoresDentroAP(valorDiaria.dentro, viagem.servidor_acompanhando) / 2;
+          return diarias * this.valorServidoresDentroAP(valorDiaria.dentro,  viagem.servidor_acompanhando) + meiaDiaria;
+        }
+    }
+
+
+    private valorServidoresDentroAP(valorDiaria: number, acompanha: string): number {    
+        if(acompanha === "SIM"){
+          return this.valorAcompanhando( Local.DENTRO);
+        }   
+        return valorDiaria;
+      }
+
+
+      private valorAcompanhando(local: Local): number {
+        switch (local) {
+          case Local.DENTRO:
+            return Acompanha.DENTRO;
+          case Local.FORA:
+            return Acompanha.FORA;
+          case Local.INTERNACIONAL:
+            return Acompanha.INTERNACIONAL;
+          default:
+            return 0;
+        }
+      }
+
+    
 
 }
