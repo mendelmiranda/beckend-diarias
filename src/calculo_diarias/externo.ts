4d0d189f295dae0df24830c3ce68297519be1a39
Wendell Miranda <wendell.sacramento@tce.ap.gov.br>
2023-09-19 09:27:32 -0300

    ajustando calculo de diárias para nacional

--- src/calculo_diarias/externo.ts
+++ src/calculo_diarias/externo.ts
@@ -1,5 +1,53 @@
+import { Util } from "src/util/Util";
+import { Acompanha, Local, Municipios, UF } from "./diarias-enum";
+import { valor_diarias, viagem, evento } from '@prisma/client';
 
 
 export default class ForaEstado {
+
+    servidores(viagem: viagem, uf: string, cidade: string, valorDiaria: valor_diarias, evento: evento): number { 
+        
+        return this.viagemNacional(viagem, uf, valorDiaria, evento );        
+      }
+  
+      private viagemNacional(viagem: viagem, uf: string, valorDiaria: valor_diarias, evento: evento): number {
+        const totalDias = Util.totalDeDias(evento.inicio, evento.fim)+2;
+        const diarias = totalDias - 1;
+
+        if (uf !== UF.AP && viagem.exterior === "NAO" && evento.tem_passagem === "SIM") {
+            const meiaDiaria = this.valorServidoresForaAP(valorDiaria.fora, viagem.servidor_acompanhando) / 2;
+            return diarias * this.valorServidoresForaAP(valorDiaria.fora, viagem.servidor_acompanhando) + meiaDiaria;
+        }
+        
+        return 0;
+      }
+  
+      valorNacional(viagem: viagem, uf: string, valorDiaria: valor_diarias): number {
+        return  this.valorServidoresForaAP(valorDiaria.fora, viagem.servidor_acompanhando); 
+      }
+
+      valorNacionalMeia(viagem: viagem, valorDiaria: valor_diarias): number {
+        return this.valorServidoresForaAP(valorDiaria.fora, viagem.servidor_acompanhando) / 2;     
+      }
+
+      private valorServidoresForaAP(valorDiaria: number, acompanha: string): number {    
+        if(acompanha === "SIM"){
+          return this.valorAcompanhando(Local.FORA);
+        }   
+        return valorDiaria;
+      }
+  
+      private valorAcompanhando(local: Local): number {
+          switch (local) {
+            case Local.DENTRO:
+              return Acompanha.DENTRO;
+            case Local.FORA:
+              return Acompanha.FORA;
+            case Local.INTERNACIONAL:
+              return Acompanha.INTERNACIONAL;
+            default:
+              return 0;
+          }
+      }
 
 }
