e9fc3cbf490216c7dce4f2f582bdf2300f280cf6
Wendell Miranda <wendell.sacramento@tce.ap.gov.br>
2023-09-26 13:20:58 -0300

    desconectando no main

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaClient, prisma } from '@prisma/client';
 
 async function bootstrap() {
+  const prisma = new PrismaClient()
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: true,
             methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
             credentials: true,
   });
-  await app.listen(4000);
+
+  await app.listen(4000).then(async () => {
+    await prisma.$disconnect();
+  }).catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+  });
 }
 bootstrap();
