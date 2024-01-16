a073bd39a2ea985319ff6b386807628e0320c1f7
Wendell Miranda <wendell.sacramento@tce.ap.gov.br>
2024-01-16 09:19:53 -0300

    ajustando para o evento na mesma cidade

--- src/calculo_diarias/externo.ts
+++ src/calculo_diarias/externo.ts
@@ -1,53 +1,60 @@
-import { Util } from "src/util/Util";
-import { Acompanha, Local, Municipios, UF } from "./diarias-enum";
+import { Util } from 'src/util/Util';
+import { Acompanha, Local, Municipios, UF } from './diarias-enum';
 import { valor_diarias, viagem, evento } from '@prisma/client';
 
-
 export default class CalculoNacional {
+  servidores(viagem: viagem, uf: string, valorDiaria: valor_diarias, evento: evento, temPassagem: string): number {
+    let mesmoDestino = false;
+    if (viagem.origem_id === viagem.destino_id) {
+      mesmoDestino = true;
+    }
+    return this.viagemNacional(viagem, uf, valorDiaria, evento, temPassagem, mesmoDestino);
+  }
 
-    servidores(viagem: viagem, uf: string, valorDiaria: valor_diarias, evento: evento, temPassagem: string): number { 
+  private viagemNacional(viagem: viagem, uf: string, valorDiaria: valor_diarias, evento: evento, temPassagem: string, mesmoDestino?: boolean): number {
+    const totalDias = Util.totalDeDias(evento.inicio, evento.fim);
+    const diarias = totalDias - 1;
 
-        return this.viagemNacional(viagem, uf, valorDiaria, evento, temPassagem );        
+    if (uf !== UF.AP && viagem.exterior === 'NAO' && temPassagem === 'SIM') {
+      let meiaDiaria = 0;
+
+      if(!mesmoDestino){
+        meiaDiaria = this.valorServidoresForaAP(valorDiaria.fora, viagem.servidor_acompanhando) / 2;
+      } else {
+        meiaDiaria = this.valorServidoresForaAP(valorDiaria.fora, viagem.servidor_acompanhando);
       }
-  
-      private viagemNacional(viagem: viagem, uf: string, valorDiaria: valor_diarias, evento: evento, temPassagem: string): number {
-        const totalDias = Util.totalDeDias(evento.inicio, evento.fim);
-        const diarias = totalDias - 1;   
-        
-        if (uf !== UF.AP && viagem.exterior === "NAO" && temPassagem === "SIM") {            
-            const meiaDiaria = this.valorServidoresForaAP(valorDiaria.fora, viagem.servidor_acompanhando) / 2;
-            return diarias * this.valorServidoresForaAP(valorDiaria.fora, viagem.servidor_acompanhando) + meiaDiaria;
-        }
-        
+      
+      return diarias * this.valorServidoresForaAP(valorDiaria.fora, viagem.servidor_acompanhando) + meiaDiaria;
+    }
+
+    return 0;
+  }
+
+  valorNacional(viagem: viagem, uf: string, valorDiaria: valor_diarias): number {
+    return this.valorServidoresForaAP(valorDiaria.fora, viagem.servidor_acompanhando);
+  }
+
+  valorNacionalMeia(viagem: viagem, valorDiaria: valor_diarias): number {
+    return this.valorServidoresForaAP(valorDiaria.fora, viagem.servidor_acompanhando) / 2;
+  }
+
+  private valorServidoresForaAP(valorDiaria: number, acompanha: string): number {
+    if (acompanha === 'SIM') {
+      return this.valorAcompanhando(Local.FORA);
+    }
+    return valorDiaria;
+  }
+
+  private valorAcompanhando(local: Local): number {
+    switch (local) {
+      case Local.DENTRO:
+        return Acompanha.DENTRO;
+      case Local.FORA:
+        return Acompanha.FORA;
+      case Local.INTERNACIONAL:
+        return Acompanha.INTERNACIONAL;
+      default:
         return 0;
-      }
-  
-      valorNacional(viagem: viagem, uf: string, valorDiaria: valor_diarias): number {
-        return  this.valorServidoresForaAP(valorDiaria.fora, viagem.servidor_acompanhando); 
-      }
-
-      valorNacionalMeia(viagem: viagem, valorDiaria: valor_diarias): number {
-        return this.valorServidoresForaAP(valorDiaria.fora, viagem.servidor_acompanhando) / 2;     
-      }
-
-      private valorServidoresForaAP(valorDiaria: number, acompanha: string): number {    
-        if(acompanha === "SIM"){
-          return this.valorAcompanhando(Local.FORA);
-        }   
-        return valorDiaria;
-      }
-  
-      private valorAcompanhando(local: Local): number {
-          switch (local) {
-            case Local.DENTRO:
-              return Acompanha.DENTRO;
-            case Local.FORA:
-              return Acompanha.FORA;
-            case Local.INTERNACIONAL:
-              return Acompanha.INTERNACIONAL;
-            default:
-              return 0;
-          }
-      }
-
+    }
+  }
 }
