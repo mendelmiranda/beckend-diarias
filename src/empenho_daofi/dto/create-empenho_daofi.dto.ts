852cf7e481df3b9873c35c83d8aefe311307ae96
Wendell Miranda <wendell.sacramento@tce.ap.gov.br>
2024-01-17 12:15:06 -0300

    adicionando compo para daof

--- src/empenho_daofi/dto/create-empenho_daofi.dto.ts
+++ src/empenho_daofi/dto/create-empenho_daofi.dto.ts
@@ -3,6 +3,7 @@ datareg: Date;
   tipo: string;
   saldo_inicial: number;
   valor_reservado: number;
+  valor_pos_reserva?: number;
   solicitacao_id: number;
   acao?: string;
   observacao?: string;
