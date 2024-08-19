74eac7ca7b3f3eeaf4b9191f91522d23ec0726cf
Wendell Miranda <wendell.sacramento@tce.ap.gov.br>
2024-08-19 12:14:04 -0300

    removendo um dia para ajuste da diária

--- src/calculo_diarias/externo.ts
+++ src/calculo_diarias/externo.ts
@@ -3,30 +3,25 @@ import { Acompanha, Local, Municipios, UF } from './diarias-enum';
 import { valor_diarias, viagem, evento } from '@prisma/client';
 
 export default class CalculoNacional {
-  servidores(viagem: viagem, uf: string, valorDiaria: valor_diarias, evento: evento, temPassagem: string, total: number): number {       
+  servidores(viagem: viagem, uf: string, valorDiaria: valor_diarias, evento: evento, temPassagem: string, total: number): number {
     return this.viagemNacional(viagem, uf, valorDiaria, evento, temPassagem, total);
   }
 
   private viagemNacional(viagem: viagem, uf: string, valorDiaria: valor_diarias, evento: evento, temPassagem: string, total: number): number {
     const diarias = total;
 
-    /* if(this.verificaDeslocamentoEMesmoLocal(viagem, evento)){
-      if (uf !== UF.AP && viagem.exterior === 'NAO' && temPassagem === 'SIM') {
-        return diarias * this.valorServidoresForaAP(valorDiaria.fora, viagem.servidor_acompanhando);
-      } 
-      
-    } else { */
-      if (uf !== UF.AP && viagem.exterior === 'NAO' && temPassagem === 'SIM') {
-        const meiaDiaria =  this.valorNacionalMeia(viagem, valorDiaria);      
-        return diarias * this.valorServidoresForaAP(valorDiaria.fora, viagem.servidor_acompanhando) + meiaDiaria;
-      }
-    //}
+    //const diarias = total+1;
+    if (uf !== UF.AP && viagem.exterior === 'NAO' && temPassagem === 'SIM') {
+      const meiaDiaria = this.valorNacionalMeia(viagem, valorDiaria);
+      return diarias * this.valorServidoresForaAP(valorDiaria.fora, viagem.servidor_acompanhando) + meiaDiaria;
+    }
+
 
     return 0;
   }
 
-  verificaDeslocamentoEMesmoLocal(viagem: viagem, evento: evento): boolean{
-    if(evento.cidade_id === viagem.cidade_destino_id || viagem.deslocamento === "SIM"){
+  verificaDeslocamentoEMesmoLocal(viagem: viagem, evento: evento): boolean {
+    if (evento.cidade_id === viagem.cidade_destino_id || viagem.deslocamento === "SIM") {
       return true;
     }
     return false;
