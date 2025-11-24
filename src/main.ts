06d6e6e41a4856f4b384a51cc41a9df681fb856c
Wendell Miranda Sacramento <wendell.sacramento@tce.ap.gov.br>
2025-11-24 08:34:33 -0300

    ajuste para o ip 18

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,7 @@ async function bootstrap() {
       'http://192.168.210.71:3000',
       'http://10.10.5.210:3000',
       'http://10.10.3.5:3000',
+      'http://10.10.0.18:3004',
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
